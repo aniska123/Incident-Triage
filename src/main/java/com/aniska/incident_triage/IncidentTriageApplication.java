99388fcc90a4da86ac2428a6147e6319a2df2c2a
aniska123 <nayakaniska918@gmail.com>
2026-03-12 01:03:43 +0530

    chore: add main application entry point and Swagger configuration

---
+++ src/main/java/com/aniska/incident_triage/IncidentTriageApplication.java
@@ -0,0 +1,17 @@
+package com.aniska.incident_triage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@EnableScheduling
+public class IncidentTriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentTriageApplication.class, args);
+	}
+
+}
